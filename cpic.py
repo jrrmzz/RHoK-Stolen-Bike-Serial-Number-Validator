587da0b34c2dace9c9506cb2c7ad4d7663714eaf
jrrmzz <jrrmzz@gmail.com>
2010-12-04 16:49:27 -0500

    Added changes to cpic.py (forgotten in last push)

--- cpic.py
+++ cpic.py
@@ -35,7 +35,33 @@ def scrape(serial_number):
         entries.append(entry)
     return entries
 
+def format_result(template_path, replacement_token, entries):
+    template = open(template_path, 'rU').read()
+    
+    row_template = '''
+    <ul class='field'>
+        <li><h3>Status:</h3> {Status}</li>
+        <li><h3>Color:</h3> {Color}</li>
+        <li><h3>Make</h3> {Make}</li>
+        <li><h3>Serial:</h3> {Serial}</li>
+        <li><h3>Model:</h3> {Model}</li>
+        <li><h3>Speeds:</h3> {Speeds}</li>
+    </ul>
+    '''
+    
+    if len(entries) == 0:
+        content = '<h1>This serial number has no active entries in CPIC</h1>'
+    else:
+        content = ''
+        for entry in entries:
+            content += row_template.format(**entry)
+    
+    page = template.replace(replacement_token, content)
+    return page
+
 if __name__ == '__main__':
     serial_number = 'LY22361081'
-    for entry in scrape(serial_number):
-        print entry
+    entries = scrape(serial_number)
+    print format_result('public/result.html', '<!-- $CONTENT -->', entries)
+    
+    
