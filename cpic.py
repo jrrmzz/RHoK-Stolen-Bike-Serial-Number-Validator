f07964c901734098d443430b9aecc8e277652651
jrrmzz <jrrmzz@gmail.com>
2011-01-11 11:38:37 -0500

    Added license information for Python code

--- cpic.py
+++ cpic.py
@@ -1,3 +1,28 @@
+"""CPIC Utilities
+
+A web client for performing lookups on the CPIC bicycle serial number database.
+
+Copyright 2010 The IsThisBikeStolen Team
+- Stu Basden
+- Amber Ellis
+- Jason Montojo
+- Jon Pipitone
+- Ben Shymkiw
+- John Taranu
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import urllib
 import re
 
@@ -63,5 +88,3 @@ if __name__ == '__main__':
     serial_number = 'LY22361081'
     entries = scrape(serial_number)
     print format_result('iphone/result.html', '<!-- $CONTENT -->', entries)
-    
-    
